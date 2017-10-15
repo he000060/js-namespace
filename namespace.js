33ab448bb5125edfa74676eb61d1854d9949d9b9
he000060 <32823903+he000060@users.noreply.github.com>
2017-10-15 18:37:05 -0400

    Add files via upload

---
+++ namespace.js
@@ -0,0 +1,37 @@
+// he000060 MAD9014 
+//Namespace assignment
+
+var he000060 = {
+pickColor:function(){
+var letters = '0123456789ABCDEF'.split('');
+var color='#';
+for (var i = 0; i < 6; i++ ) {
+color += letters[Math.round(Math.random() * 15)];
+}
+return color;
+},
+// This Loop I learnt from other's code //
+
+init: function(){
+
+var newDiv = 
+document.createElement("div");
+newDiv.className = "box";
+newDiv.textContent = "he000060";
+
+var boxes = document.getElementById("boxes");
+boxes.appendChild(newDiv);
+
+newDiv.addEventListener("click",function press(){
+newDiv.style.borderColor = he000060.pickColor();
+newDiv.style.backgroundColor = he000060.pickColor(); 
+});
+newDiv.addEventListener("mouseover", function highlight(){
+newDiv.classList.add("highlight");
+});
+newDiv.addEventListener("mouseout",function removelight(){
+newDiv.classList.toggle("highlight");
+newDiv.style=null;
+});
+}
+}
